f2ddf36c04cd3aecd6fc23649410c3981b7fb821
Miguel David Briceño Goncalves <miguelbricenocode@gmail.com>
2024-09-10 21:42:04 +0200

    Aprendiendo a realizar components y dividir la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Guitar from './components/Guitar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite - React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header/>
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          <Guitar/>
+          <Guitar/>
+          <Guitar/>
+          <Guitar/>
+          <Guitar/>
+          <Guitar/>
+          <Guitar/>
+        </div>
+    </main>
+    <Footer/>
     </>
   )
 }
